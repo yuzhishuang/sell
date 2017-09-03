e3db28b1c1fbdd6521076d03e4c0b484a003fdff
yuzhishuang <yzsysu@163.com>
2017-09-04 07:30:56 +0800

    9月4日早

--- src/components/header/header.vue
+++ src/components/header/header.vue
@@ -1,22 +1,26 @@
 <template>
   <div class="header">
     <div class="content-wrapper">
-	  <div class="avatar">
-	    <img width="64" height="64" :src="seller.avatar">
-	  </div>
-	  <div class="content">
-	  	<div class="title">
-	  	  <span class="brand"></span>
-	  	  <span class="name">{{seller.name}}</span>
-	  	</div>
-	  	<div class="description">
-  		  {{seller.description}}/{{seller.deliveryTime}}分钟送达
-	  	</div>
-	  	<div class="support" v-if="seller.supports">
-		  <span class="icon" :class="classMap[seller.supports[0].type]"></span>
-		  <span class="text">{{seller.supports[0].description}}</span>
-	  	</div>
-	  </div>
+		  <div class="avatar">
+		    <img width="64" height="64" :src="seller.avatar">
+		  </div>
+		  <div class="content">
+		  	<div class="title">
+		  	  <span class="brand"></span>
+		  	  <span class="name">{{seller.name}}</span>
+		  	</div>
+		  	<div class="description">
+	  		  {{seller.description}}/{{seller.deliveryTime}}分钟送达
+		  	</div>
+		  	<div class="support" v-if="seller.supports">
+			    <span class="icon" :class="classMap[seller.supports[0].type]"></span>
+			    <span class="text">{{seller.supports[0].description}}</span>
+		  	</div>
+		  </div>
+		  <div v-if="seller.supports" class="support-count">
+		  	<span class="count">{{seller.supports.length}}个</span>
+		  	<i class="icon-keyboard_arrow_right"></i>
+		  </div>
     </div>
   </div>
 </template>
@@ -39,8 +43,9 @@
 
   .header
     color: #fff
-    background: #000
+    background: #999
     .content-wrapper
+      position: relative
       padding: 24px 12px 18px 24px
       font-size: 0
       .avatar
@@ -50,6 +55,7 @@
           border-radius: 2px
       .content
         display: inline-block
+        position: relative
         margin-left: 16px
         font-size: 14px
         .title
@@ -94,4 +100,21 @@
           .text
             line-height: 12px
             font-size: 10px
+    	.support-count
+    	  position: absolute
+    	  right: 12px
+    	  bottom: 18px
+    	  padding: 0 8px
+    	  height: 24px
+    	  line-height: 24px
+    	  border-radius: 14px
+    	  background: rgba(0,0,0,0.2)
+    	  text-align: center
+    	  .count
+    	    vertical-align: center
+    	    font-size: 10px
+	      .icon-keyboard_arrow_right
+	        margin-left: 2px
+	        line-height: 24px
+	        font-size: 10px
 </style>
